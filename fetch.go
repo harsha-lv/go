04323a28d22fb51279d8605cb076df72824c1124
harsha-lv <harsha.2006@gmail.com>
2026-08-18 00:02:05 +0530

    implemnt http status code

--- fetch.go
+++ fetch.go
@@ -5,15 +5,20 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main(){
 	for _, url := range os.Args[1:]{
+		if !strings.HasPrefix(url, "http://"){
+			url = "http://"+url
+		}
 		resp, err := http.Get(url)
 		if err!=nil{
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
+		fmt.Fprintf(os.Stderr, "status: %s (%d)\n", resp.Status, resp.StatusCode)
 		n, err := io.Copy(os.Stdout, resp.Body)
 		resp.Body.Close()
 		if err != nil{
